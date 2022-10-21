6d93f2e6c0e0d63868171b05c69945cf3b61153d
sgraziola <simo.fdl@gmail.com>
2022-10-21 12:22:50 +0200

    Snack2: add tolowercase

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -1,3 +1,19 @@
 /* Snack 2
 A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
 Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+
+const soccerTeams = ["MIlan","juvENtus","iNTer","napoLI"];
+//scorro nell'array
+soccerTeams.forEach((team) => {
+    //per ogni elemento mando tutta la stringa in lower case
+    const teamLower = team.toLowerCase();
+    //selezione prima lettera all'indice 0
+    teamLower.forEach((letter) => {
+        letter[0].toUpperCase; 
+        console.log(teamLower);
+    
+    });
+    
+    
+});
